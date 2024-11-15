dbaeeb81dc34ecd2344d1a6a4e3e3caee7179241
Bambi Brewer <robotbambi@gmail.com>
2024-11-15 16:26:33 -0500

    uninstall dotenv

--- app/config.ts
+++ app/config.ts
@@ -1,2 +1,3 @@
 export const OPENAI_API_KEY = process.env.NEXT_PUBLIC_OPENAI_API_KEY;
 export const TAVILY_API_KEY = process.env.NEXT_PUBLIC_TAVILY_API_KEY;
+export const LANGCHAIN_API_KEY = process.env.NEXT_PUBLIC_LANGCHAIN_API_KEY;
